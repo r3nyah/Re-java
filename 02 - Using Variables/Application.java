fad8e1c308e885a82ceaca2948c3d06627479c8a
r3nyah <88012298+r3nyah@users.noreply.github.com>
2024-07-19 19:51:26 +0700

    Session 1

---
+++ 02 - Using Variables/Application.java
@@ -0,0 +1,24 @@
+public class Application {
+    public static void main(String[] args) {
+        int myNumber = 20;
+        short myShort = 2005;
+        long myLong = 10;
+
+        double myDouble = 20.0;
+        float myFloat = 20.f;
+
+        char myChar = 'G';
+        boolean myBoolean = true;
+
+        byte myByte =  10;
+
+        System.out.println(myNumber);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(myByte);
+    }
+}
---
+++ 03 - Working With Text/Application.java
@@ -0,0 +1,23 @@
+public class Application {
+    public static void main(String[] args) {
+        int myInt = 10;
+        
+        String text = "Hello";
+        
+        String blank = " ";
+
+        String name = "GB";
+
+        String greeting = text + blank + name;
+
+        System.out.println(greeting);
+
+        System.out.println("Hello" + " " + "GB");
+
+        System.out.println("My integer is: " +  myInt);
+
+        double myDouble = 10.20;
+
+        System.out.println("My number is: " + myDouble + " ");
+    }
+}
