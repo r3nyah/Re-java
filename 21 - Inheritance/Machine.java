2bfb99d671b5c2c8d9a67c489491595a847247d3
r3nyah <arianfebrian46@gmail.com>
2024-08-04 15:26:53 +0700

    Session 6

---
+++ 21 - Inheritance/Machine.java
@@ -0,0 +1,11 @@
+public class Machine {
+    protected String name = "Machine Type 1";
+
+    public void start(){
+        System.out.println("Machine Started");
+    }
+
+    public void stop(){
+        System.out.println("Machine Stopped");
+    }
+}
